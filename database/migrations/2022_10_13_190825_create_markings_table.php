dd1a8f44b2f0d4a4d67ccdc2f5478bd4d8635bc0
Mohiuddin Tamim <mohiuhere@gmail.com>
2022-10-15 00:16:48 +0600

    show marking work done a bit bug needed to fix: fix(database migration was faulty)

--- database/migrations/2022_10_13_190825_create_markings_table.php
+++ database/migrations/2022_10_13_190825_create_markings_table.php
@@ -16,10 +16,13 @@ return new class extends Migration
         Schema::create('markings', function (Blueprint $table) {
             $table->id();
             $table->string('review_status');
+
             $table->unsignedBigInteger('marking_submission_id');
             $table->foreign('marking_submission_id')->references('id')->on('submissions');
-            $table->unsignedBigInteger('marking_review_id');
-            $table->foreign('marking_review_id')->references('id')->on('reviews');
+
+            $table->unsignedBigInteger('marking_review_user_id');
+            $table->foreign('marking_review_user_id')->references('id')->on('users');
+
             $table->unsignedBigInteger('marking_conference_id');
             $table->foreign('marking_conference_id')->references('id')->on('conferences');
             $table->timestamps();
