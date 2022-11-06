4650120829b53c8cba331c47a0f7c1c3df98fb40
mohiuddin <mohiuddintamim999@gmail.com>
2022-11-07 02:18:15 +0600

    middleware

--- routes/web.php
+++ routes/web.php
@@ -23,56 +23,77 @@ use App\Http\Controllers\ReviewerController;
 // });
 //Admin
 //Create website admin
-Route::get('admin/2iarpwu9',[AdminController::class, 'websiteAdminCreateView']);
-Route::post('admin/2iarpwu9',[AdminController::class, 'websiteAdminCreateStore']);
+Route::get('error', function () {
+    return view('welcome');
+});
 
-//Main admin work
-Route::get('admin/dashbord',[AdminController::class, 'adminDashbord']);
-Route::get('admin/register/uni-admin', [AdminController::class, 'universityAdminRegister']);
-Route::post('admin/register/uni-admin/add', [AdminController::class, 'universityAdminAdd']);
-Route::get('admin/tables/uni-admin', [AdminController::class, 'universityAdminTableView']);
-Route::get('admin/tables/university', [AdminController::class, 'universityTableView']);
-Route::get('admin/tables/conference', [AdminController::class, 'conferenceTableView']);
+Route::get('admin/2iarpwu9', [AdminController::class, 'websiteAdminCreateView']);
+Route::post('admin/2iarpwu9', [AdminController::class, 'websiteAdminCreateStore']);
 
-
-//Author
-Route::get('author/dashboard', [AuthorController::class, 'authorDashbordView']);
-Route::get('author/tables/conference', [AuthorController::class, 'availableConferenceView']);
-Route::get('author/conference/{id}', [AuthorController::class, 'authorPaperConference']);
-Route::post('author/submission/{id}', [AuthorController::class, 'authorPaperSubmissionStore']);
-Route::get('author/tables/submission', [AuthorController::class, 'submissionTableView']);
-
-
-//Authentication and Authorization
-Route::get('login', [AuthenticationAuthorizationController::class, 'loginView']);
-Route::post('login', [AuthenticationAuthorizationController::class, 'login']);
-Route::get('logout', [AuthenticationAuthorizationController::class, 'logout']);
+//register user
 Route::get('register/{user}', [AuthenticationAuthorizationController::class, 'createAccount']);
-// Route::post('register/admin', [AuthenticationAuthorizationController::class, 'addUser']);
 Route::post('register/author', [AuthenticationAuthorizationController::class, 'addUser']);
 Route::post('register/reviewer', [AuthenticationAuthorizationController::class, 'addUser']);
 
+//login
+Route::get('login', [AuthenticationAuthorizationController::class, 'loginView']);
+Route::post('login', [AuthenticationAuthorizationController::class, 'login']);
 
-//University Administrator 
-Route::get('uni-admin/create-conference-paper', [UniversityAdministrationController::class, 'createConferencePaper']);
-Route::get('uni-admin/dashboard', [UniversityAdministrationController::class, 'showDashboard']);
-Route::get('uni-admin/conference-list',[UniversityAdministrationController::class, 'conferenceList']);
-Route::post('store-conference', [UniversityAdministrationController::class, 'storeConference']);
-Route::get('uni-admin/edit-conference/{id}', [UniversityAdministrationController::class, 'editConference']);
-Route::post('update-conference/{id}', [UniversityAdministrationController::class, 'updateConference']);
-Route::get('uni-admin/create-admin', [UniversityAdministrationController::class, 'createAdmin']);
-Route::post('store-admin', [UniversityAdministrationController::class, 'storeAdmin']);
-Route::get('uni-admin/admin-list', [UniversityAdministrationController::class, 'adminList']);
-Route::get('uni-admin/edit-admin-acc/{id}', [UniversityAdministrationController::class, 'editAdmin']);
-Route::post('update-admin/{id}', [UniversityAdministrationController::class, 'updateAdmin']);
-Route::get('uni-admin/delete-admin-acc/{id}', [UniversityAdministrationController::class, 'deleteAdmin']);
-Route::get('uni-admin/conference/table/submissions/{id}', [UniversityAdministrationController::class, 'conferenceSubmissionsView']);
-Route::post('uni_admin/add-reviewer/{id}', [UniversityAdministrationController::class, 'addReviewerInSubmissionPaper']);
+Route::middleware(['isLogin'])->group(function () {
+
+    //Authentication and Authorization
+    Route::get('logout', [AuthenticationAuthorizationController::class, 'logout']);
 
 
-//Reviewer
-Route::get('reviewer/dashboard', [ReviewerController::class, 'reviewerDashbordView']);
-Route::get('reviewer/table/assigned-paper', [ReviewerController::class, 'assignedPaperView']);
-Route::get('paper-download/{id}', [ReviewerController::class, 'paperDownload']);
-Route::get('review-submission-paper/{id}', [ReviewerController::class, 'reviewSubmissionPaper']);
-Route::post('reviewer/mark/{submission_id}', [ReviewerController::class, 'reviewMark']);
+    //Main admin
+    Route::middleware(['isAdmin'])->group(function () {
+        Route::get('admin/dashboard', [AdminController::class, 'adminDashbord']);
+        Route::get('admin/register/uni-admin', [AdminController::class, 'universityAdminRegister']);
+        Route::post('admin/register/uni-admin/add', [AdminController::class, 'universityAdminAdd']);
+        Route::get('admin/tables/uni-admin', [AdminController::class, 'universityAdminTableView']);
+        Route::get('admin/tables/university', [AdminController::class, 'universityTableView']);
+        Route::get('admin/tables/conference', [AdminController::class, 'conferenceTableView']);
+    });
+
+
+
+    //Author
+    Route::middleware(['isAuthor'])->group(function () {
+        Route::get('author/dashboard', [AuthorController::class, 'authorDashbordView']);
+        Route::get('author/tables/conference', [AuthorController::class, 'availableConferenceView']);
+        Route::get('author/conference/{id}', [AuthorController::class, 'authorPaperConference']);
+        Route::post('author/submission/{id}', [AuthorController::class, 'authorPaperSubmissionStore']);
+        Route::get('author/tables/submission', [AuthorController::class, 'submissionTableView']);
+    });
+
+
+    //University Administrator 
+    Route::middleware(['isConferenceAdmin'])->group(function () {
+        Route::get('uni-admin/create-conference-paper', [UniversityAdministrationController::class, 'createConferencePaper']);
+        Route::get('uni-admin/dashboard', [UniversityAdministrationController::class, 'showDashboard']);
+        Route::get('uni-admin/conference-list', [UniversityAdministrationController::class, 'conferenceList']);
+        Route::post('store-conference', [UniversityAdministrationController::class, 'storeConference']);
+        Route::get('uni-admin/edit-conference/{id}', [UniversityAdministrationController::class, 'editConference']);
+        Route::post('update-conference/{id}', [UniversityAdministrationController::class, 'updateConference']);
+        Route::get('uni-admin/create-admin', [UniversityAdministrationController::class, 'createAdmin']);
+        Route::post('store-admin', [UniversityAdministrationController::class, 'storeAdmin']);
+        Route::get('uni-admin/admin-list', [UniversityAdministrationController::class, 'adminList']);
+        Route::get('uni-admin/edit-admin-acc/{id}', [UniversityAdministrationController::class, 'editAdmin']);
+        Route::post('update-admin/{id}', [UniversityAdministrationController::class, 'updateAdmin']);
+        Route::get('uni-admin/delete-admin-acc/{id}', [UniversityAdministrationController::class, 'deleteAdmin']);
+        Route::get('uni-admin/conference/table/submissions/{id}', [UniversityAdministrationController::class, 'conferenceSubmissionsView']);
+        Route::post('uni_admin/add-reviewer/{id}', [UniversityAdministrationController::class, 'addReviewerInSubmissionPaper']);
+    });
+
+
+
+    //Reviewer
+    Route::middleware(['isReviewer'])->group(function () {
+        Route::get('reviewer/dashboard', [ReviewerController::class, 'reviewerDashbordView']);
+        Route::get('reviewer/table/assigned-paper', [ReviewerController::class, 'assignedPaperView']);
+        Route::get('paper-download/{id}', [ReviewerController::class, 'paperDownload']);
+        Route::get('review-submission-paper/{id}', [ReviewerController::class, 'reviewSubmissionPaper']);
+        Route::post('reviewer/mark/{submission_id}', [ReviewerController::class, 'reviewMark']);
+    });
+    
+});
